21059d8d73d27d5a68be0b71682293bb626d465f
Aleks20x <alekssandro60@gmail.com>
2026-04-18 18:46:46 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,35 @@
 const cacheName = 'zero-g-v1';
 const assets = ['./', './index.html', './manifest.json'];
 
+// Evento de Instalação
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  // Força o Service Worker a se tornar o ativo imediatamente, sem esperar as abas fecharem
+  self.skipWaiting();
+  
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Evento de Ativação (Limpeza de caches antigos)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Evento de Fetch (Recuperação de dados)
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
